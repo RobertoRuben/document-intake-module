35f20855ad0897310e7cd25bfdb7d0f7b4a0ce88
RobertoRuben <robertoch263@gmail.com>
2025-04-23 17:10:19 -0500

    ✨ feat(auth): add AuthPage and AppRouter components; refactor App to utilize AppRouter for routing

---
+++ src/modules/routes/AppRouter.tsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import { Layout } from "@/modules/core/structure/layout/Layout";
+import AuthPage from "@/modules/auth/AuthPage";
+
+export function AppRouter() {
+  return (
+    <Routes>
+      <Route path="/auth" element={<AuthPage />} />
+      
+      <Route path="/" element={<Layout />}>
+      </Route>
+    </Routes>
+  );
+}
