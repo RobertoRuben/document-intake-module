60f92a53762c1d5fb4efa9a955734cd9559c22a2
RobertoRuben <robertoch263@gmail.com>
2025-04-23 17:10:27 -0500

    ✨ feat(auth): refactor App component to utilize AppRouter for routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,14 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Layout } from "./modules/core/structure/layout/Layout";
+import { BrowserRouter } from "react-router-dom";
 import { LoadingSpinnerProvider } from "./modules/core/structure/loading-spinner/providers/LoadingSpinnerProvider";
+import { AppRouter } from "./modules/routes/AppRouter";
 
 function App() {
   return (
-    <LoadingSpinnerProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            {/* Aquí van las rutas anidadas que se mostrarán dentro del Layout a través del Outlet */}
-            <Route index element={<div className="p-6">Página de inicio</div>} />
-            <Route path="/inicio" element={<div className="p-6">Contenido de inicio</div>} />
-            <Route path="/notificaciones" element={<div className="p-6">Notificaciones</div>} />
-            <Route path="*" element={<div className="p-6">Página no encontrada</div>} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </LoadingSpinnerProvider>
+      <LoadingSpinnerProvider>
+        <BrowserRouter>
+          <AppRouter />
+        </BrowserRouter>
+      </LoadingSpinnerProvider>
   );
 }
 
