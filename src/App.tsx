a8058a63ce746d0e79b668b864964774e0fd89fc
RobertoRuben <robertoch263@gmail.com>
2025-04-25 07:38:31 -0500

    ✨ feat(auth): wrap AppRouter with AuthProvider and add Toaster for notifications

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import { BrowserRouter } from "react-router-dom";
 import { LoadingSpinnerProvider } from "./modules/core/structure/loading-spinner/providers/LoadingSpinnerProvider";
 import { AppRouter } from "./modules/routes/AppRouter";
+import { AuthProvider } from "./modules/auth/context/AuthContext";
+import {Toaster} from "sonner";
 
 function App() {
-  return (
-      <LoadingSpinnerProvider>
-        <BrowserRouter>
-          <AppRouter />
-        </BrowserRouter>
-      </LoadingSpinnerProvider>
-  );
+    return (
+        <LoadingSpinnerProvider>
+            <BrowserRouter>
+                <AuthProvider>
+                    <AppRouter />
+                    <Toaster position="top-right" richColors />
+                </AuthProvider>
+            </BrowserRouter>
+        </LoadingSpinnerProvider>
+    );
 }
 
 export default App;
