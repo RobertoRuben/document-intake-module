269b5251a84b1fa29bbc812b2250c53649785159
RobertoRuben <robertoch263@gmail.com>
2025-04-25 12:33:55 -0500

    ✨ feat(roles): add RolePage component to render RoleContainer

---
+++ src/modules/roles/pages/RolePage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { RoleContainer} from "@/modules/roles/components/role-container/RoleContainer.tsx";
+
+const RolePage: React.FC = () => {
+    return <RoleContainer />;
+};
+
+export default RolePage;
