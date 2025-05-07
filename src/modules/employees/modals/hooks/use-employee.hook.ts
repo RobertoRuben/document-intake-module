522b86ec3a091836ec60acc4c1e6ee612fd132ad
RobertoRuben <robertoch263@gmail.com>
2025-05-07 15:00:02 -0500

    ✨ feat(employees): add useEmployeeForm hook and employeeFormSchema for employee data validation

---
+++ src/modules/employees/modals/hooks/use-employee.hook.ts
@@ -0,0 +1,38 @@
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Employee } from "../../models/employee.model";
+import { employeeFormSchema, EmployeeFormValues } from "../validators/employee.validator.schema";
+
+export const useEmployeeForm = (employee?: Employee) => {
+    const form = useForm<EmployeeFormValues>({
+        resolver: zodResolver(employeeFormSchema),
+        defaultValues: {
+            dni: 0,
+            names: "",
+            paternalSurname: "",
+            maternalSurname: "",
+            gender: "",
+            positionId: 0,
+            departmentId: 0
+        },
+    });
+
+    useEffect(() => {
+        if (employee) {
+            form.reset({
+                dni: employee.dni,
+                names: employee.names,
+                paternalSurname: employee.paternalSurname,
+                maternalSurname: employee.maternalSurname,
+                gender: employee.gender || "",
+                positionId: employee.positionId,
+                departmentId: employee.departmentId
+            });
+        }
+    }, [employee, form]);
+
+    return {
+        form,
+    };
+};
