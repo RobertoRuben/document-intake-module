e6f3c2feaa8e5a0f936c34f4b7c5c0edc18fa768
RobertoRuben <robertoch263@gmail.com>
2025-05-07 11:42:49 -0500

    ✨ feat(router): agregar el componente AppRouter para gestionar las rutas de la aplicación

---
+++ src/routes/app.router.tsx
@@ -0,0 +1,28 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import { Layout } from "@/modules/core/structure/layout/Layout";
+import { AuthRoutes } from "@/modules/auth/routes/auth.routes.tsx";
+import { ProtectedRoute } from "@/modules/auth/routes/protected-route.component.tsx";
+import { RoleRoutes} from "@/modules/roles/routes/role.router.tsx";
+import { DepartmentRoutes } from "@/modules/departments/routes/department.router.tsx";
+import { PositionRoutes } from "@/modules/positions/routes/position.router.tsx";
+
+export function AppRouter() {
+    return (
+        <Routes>
+            {AuthRoutes}
+
+            <Route element={<ProtectedRoute />}>
+                <Route path="/" element={<Layout />}>
+                    {RoleRoutes}
+                    {DepartmentRoutes}
+                    {PositionRoutes}
+                </Route>
+            </Route>
+
+            <Route
+                path="*"
+                element={<Navigate to="/auth" replace />}
+            />
+        </Routes>
+    );
+}
