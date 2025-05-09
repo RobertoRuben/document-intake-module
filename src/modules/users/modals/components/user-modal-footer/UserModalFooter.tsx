5cc2db8b5f0d61dc3405fb5e8e17ab520868e083
RobertoRuben <robertoch263@gmail.com>
2025-05-09 14:39:10 -0500

    ✨ feat(users): add UserModalFooter component for improved modal functionality

---
+++ src/modules/users/modals/components/user-modal-footer/UserModalFooter.tsx
@@ -0,0 +1,22 @@
+import { DialogFooter } from "@/modules/core/components/ui/dialog";
+import { ModalCancelButton } from "@/globals/components/ModalCancelButton";
+import { ModalSubmitButton } from "@/globals/components/ModalSubmitButton";
+
+interface UserModalFooterProps {
+    isEditing: boolean;
+    onClose: () => void;
+    onSubmit: () => void;
+}
+
+export const UserModalFooter: React.FC<UserModalFooterProps> = ({ isEditing, onClose, onSubmit }) => {
+    return (
+        <DialogFooter className="flex flex-col w-full space-y-3 sm:space-y-0">
+            <div className="w-full sm:mb-3">
+                <ModalCancelButton onClose={onClose} />
+            </div>
+            <div className="w-full">
+                <ModalSubmitButton isEditing={isEditing} onSubmit={onSubmit} />
+            </div>
+        </DialogFooter>
+    );
+};
