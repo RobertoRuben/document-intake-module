b24d794fd92b1dfa16377b4712e4dfd5f0f0f4f9
RobertoRuben <robertoch263@gmail.com>
2025-05-23 16:28:46 -0500

    ✨ feat(documentary-topic-router): agregar rutas para la página de temas documentales

---
+++ src/modules/documentary-topic/routes/documentary-topic.router.tsx
@@ -0,0 +1,6 @@
+import { Route } from "react-router-dom";
+import DocumentaryTopicPage from "../pages/DocumentaryTopicPage";
+
+export const DocumentaryTopicRoutes = [
+    <Route key="documentary-topics" path="/documentary-topics" element={<DocumentaryTopicPage />} />
+];
