48f8ff2c1f5cee544842d9f5ef59c3d3959df53c
RobertoRuben <robertoch263@gmail.com>
2025-05-23 15:27:49 -0500

    ✨ feat(documentary-topic-modal-form): add DocumentaryTopicModalForm component for managing documentary topic data

---
+++ src/modules/documentary-topic/modals/DocumentaryTopicModal.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { Form } from "@/modules/core/components/ui/form";
+import { DocumentaryTopic } from "@/modules/documentary-topic/model/documentary-topic-model";
+import { DocumentaryTopicModalFooter } from "./components/documentary-topic-modal-footer/DocumentaryTopicModalFooter";
+import { DocumentaryTopicFormFields } from "./components/documentary-topic-modal-form/DocumentaryTopicModalFormFields";
+import { useDocumentaryTopicForm } from "./hooks/use-documentary-topic.hook";
+import { DocumentaryTopicFormValues } from "@/modules/documentary-topic/modals/validators/documentary-topic.validatos.schema";
+
+interface DocumentaryTopicModalFormProps {
+    documentaryTopic?: DocumentaryTopic;
+    isEditing: boolean;
+    onClose: () => void;
+    onSubmit: (data: DocumentaryTopic) => Promise<boolean>;
+}
+
+export const DocumentaryTopicModalForm: React.FC<DocumentaryTopicModalFormProps> = ({
+    documentaryTopic,
+    isEditing,
+    onClose,
+    onSubmit,
+}) => {
+    const { form } = useDocumentaryTopicForm(documentaryTopic);
+
+    const handleSubmit = async (values: DocumentaryTopicFormValues) => {
+        const topicData: DocumentaryTopic = {
+            id: documentaryTopic?.id || undefined,
+            name: values.name,
+        };
+
+        const success = await onSubmit(topicData);
+        if (success) {
+            onClose();
+        }
+    };
+
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(handleSubmit)} className="p-6">
+                <div className="mb-6">
+                    <DocumentaryTopicFormFields form={form} />
+                </div>
+                <DocumentaryTopicModalFooter
+                    isEditing={isEditing}
+                    onClose={onClose}
+                    onSubmit={form.handleSubmit(handleSubmit)}
+                />
+            </form>
+        </Form>
+    );
+};
